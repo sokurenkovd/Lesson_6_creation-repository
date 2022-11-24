205fc9d84729fc3445e9962266c7ac0285422088
sokurenkovd <118994653+sokurenkovd@users.noreply.github.com>
2022-11-24 18:38:34 +0300

    1st changes
    1st changes

---
+++ Hello_world/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        int first = 18;
+        int second = 6;
+        int answer = first + second;
+        Console.WriteLine(answer);
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ branches/Program.cs
@@ -0,0 +1,8 @@
+﻿int sum = 0;
+for (int x = 1; x<20; x++){
+    if (x%3 == 0){
+        sum=sum+x;
+        Console.WriteLine($"current summary = {sum}");
+    }
+}
+Console.WriteLine($"Summa ravna = {sum}");
